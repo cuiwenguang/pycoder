ffaa7c52d97821e1982a8832a57a9d6de4e63441
cwg <cuiwenguang@gsoft.cc>
2018-01-15 17:48:55 +0800

    test

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, re_path
 
-from .views import index
+from .views import index, course, question_and_answer
 
 urlpatterns = [
-    path('', index)
+    path('', index, name="index"),
+    re_path('course(/(?P<c_name>\w+)?/)?', course, name="course"),
+    path('Q&A/', question_and_answer),
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'app/index.html')
+
+def course(request, c_name=None):
+    return render(request, 'app/courselist.html')
+
+def question_and_answer(request):
+    return render(request, 'app/qa.html')
+
