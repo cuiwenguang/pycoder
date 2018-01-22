14ae6be5c84ce7e2cc0daa0f4ac8ab4e17fb7aa0
cwg <cuiwenguang@gsoft.cc>
2018-01-22 18:02:19 +0800

    member

--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    mobile = models.CharField(max_length=20, null=True, blank=True)  # 手机
+    email = models.CharField(max_length=50, null=True, blank=True)  # 邮箱
+    name = models.CharField(max_length=20, null=True, blank=True)  # 姓名
+    avatar = models.CharField(max_length=255, null=True, blank=True)  # 头像
+    open_id = models.CharField(max_length=50, default='')  # 第三方登陆open_id
+    provider = models.CharField(max_length=10)  # 第三方认证提供者
+    points = models.IntegerField(default=0)  # 用户积分
+
+    def __str__(self):
+        return self.name
+
+
+channels = ["java", "python", "前端"]
+
+class Series(models.Model):
+    ''' 课程系列 '''
+    ser_name = models.CharField(max_length=20)
+    ser_remark = models.CharField(max_length=255)
+    part_count = models.IntegerField(default=1)
+    cover_img = models.CharField(max_length=255)
+    teacher = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL)
+
+
+class Course(models.Model):
+    category = models.ForeignKey(Series, on_delete=models.SET_DEFAULT)
+    course_name = models.CharField(max_length=50)
+    course_index = models.IntegerField(default=0)
+    course_remark = models.CharField(max_length=500, null=True, blank=True)
+
--- app/views.py
+++ app/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'app/index.html')
+    return render(request, 'app/index.html')
+
 
 def course(request, c_name=None):
     return render(request, 'app/courselist.html')
 
+
 def question_and_answer(request):
     return render(request, 'app/qa.html')
+
 
